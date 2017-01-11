450de0f17c10d4ea9e54d13a6b90587845510649
Rafał Palej <mail.rafixwpt@gmail.com>
2017-01-11 20:05:44 +0100

    Added IInstitutionServiceInterface

---
+++ InstitutionService/IInstitutionService.cs
@@ -0,0 +1,15 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Syndication;
+using Domain;
+
+namespace InstitutionService
+{
+    [ServiceContract]
+    public interface IInstitutionService
+    {
+        [OperationContract]
+        string GetInstitutionName();
+        [OperationContract]
+        Patient GetPatientBasicInfo(string pesel);
+    }
+}
