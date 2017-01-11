d7f884fa8e0675c16174b8aa3059d986f3885124
mailr <mailr@IKAROS-DESKTOP>
2017-01-11 19:27:38 +0100

    Add project files.

---
+++ WebsiteApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebsiteApplication.Startup))]
+namespace WebsiteApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
