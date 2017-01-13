15cee61f127e1f8e52a24c904ef081556184fffa
Rafał Palej <mail.rafixwpt@gmail.com>
2017-01-13 19:41:17 +0100

    Automapper Initialization
    Related Work Items: #14

--- WebsiteApplication/Global.asax.cs
+++ WebsiteApplication/Global.asax.cs
@@ -5,10 +5,12 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using AutoMapper;
+using Domain;
 
 namespace WebsiteApplication
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
@@ -16,6 +18,11 @@ namespace WebsiteApplication
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<Institution, InstitutionTransferObject>();
+            });
         }
     }
 }
