b326d38ca4fb1aa33f82a3df503378b2925e2def
Rafał Palej <mail.rafixwpt@gmail.com>
2017-01-13 20:09:23 +0100

    SimpleInjectorInitialization, AutomapperInitialization, Connected with EntityFramework

---
+++ WebsiteApplication/DataAccessLayer/IApplicationDataProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebsiteApplication.DataAccessLayer
+{
+    interface IApplicationDataProvider
+    {
+        WebsiteDatabaseContext Database { get; }
+    }
+}
--- WebsiteApplication/Global.asax.cs
+++ WebsiteApplication/Global.asax.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using AutoMapper;
 using Domain;
+using SimpleInjector;
+using SimpleInjector.Integration.Web;
 
 namespace WebsiteApplication
 {
