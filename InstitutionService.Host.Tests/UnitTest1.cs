d786bff6172f96a1df7b87c579e1f3948db6a338
Rafał Palej <mail.rafixwpt@gmail.com>
2017-01-13 22:08:55 +0100

    Tests

---
+++ InstitutionService.Host.Tests/UnitTest1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Domain.Interfaces;
+using InstitutionService.Host.Code.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InstitutionService.Host.Tests
+{
+    [TestClass]
+    public class RepositoryGetter
+    {
+        [TestMethod]
+        public void InitializeObjectBuilder()
+        {
+            ObjectBuilder.Initialize();
+        }
+
+        [TestMethod]
+        public void GetPatientRepository()
+        {
+            var repository = ObjectBuilder.Container.GetInstance<IPatientRepository>();
+            Assert.IsNotNull(repository);
+        }
+
+        [TestMethod]
+        public void GetHospitalizationRepository()
+        {
+            var repository = ObjectBuilder.Container.GetInstance<IHospitalizationRepository>();
+            Assert.IsNotNull(repository);
+        }
+
+        [TestMethod]
+        public void GetExaminationRepository()
+        {
+            var repository = ObjectBuilder.Container.GetInstance<IExaminationRepository>();
+            Assert.IsNotNull(repository);
+        }
+
+        [TestMethod]
+        public void GetTreatmentRepository()
+        {
+            var repository = ObjectBuilder.Container.GetInstance<ITreatmentRepository>();
+            Assert.IsNotNull(repository);
+        }
+
+        [TestMethod]
+        public void GetAddressRepository()
+        {
+            var repository = ObjectBuilder.Container.GetInstance<IAddressRepository>();
+            Assert.IsNotNull(repository);
+        }
+
+        [TestMethod]
+        public void GetMedicineRepository()
+        {
+            var repository = ObjectBuilder.Container.GetInstance<IMedicineRepository>();
+            Assert.IsNotNull(repository);
+        }
+
+        [TestMethod]
+        public void GetUsedMedicineRepository()
+        {
+            var repository = ObjectBuilder.Container.GetInstance<IUsedMedicineRepository>();
+            Assert.IsNotNull(repository);
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            ObjectBuilder.CleanUp();
+        }
+    }
+}
