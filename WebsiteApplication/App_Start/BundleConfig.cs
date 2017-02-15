7548c04136b5aa9600e58ba3dda20f7fb14b13b0
Rafał Palej <mail.rafixwpt@gmail.com>
2017-02-15 20:09:35 +0100

    Przejscie na KendoUI

--- WebsiteApplication/App_Start/BundleConfig.cs
+++ WebsiteApplication/App_Start/BundleConfig.cs
@@ -14,8 +14,6 @@ namespace WebsiteApplication
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
@@ -23,9 +21,20 @@ namespace WebsiteApplication
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/kendo").Include(
+                "~/Scripts/kendo/kendo.all.min.js",
+                // "~/Scripts/kendo/kendo.timezones.min.js", // uncomment if using the Scheduler
+                "~/Scripts/kendo/kendo.aspnetmvc.min.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/kendo/css").Include(
+            "~/Content/kendo/kendo.common-bootstrap.min.css",
+            "~/Content/kendo/kendo.bootstrap.min.css"));
+
+            bundles.IgnoreList.Clear();
         }
     }
 }
